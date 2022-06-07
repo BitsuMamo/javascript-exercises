edcc5b1b795a50345ef55ff554d8e1d3c77ee030
Bitsu A. Mamo <bitsua.mamo@gmail.com>
2022-06-07 17:20:12 +0300

    Repeast the string a certain amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(value, times) {
+  let result = "";
+  while(times != 0){
+    result += value;
+    times--;
+  }
 
+  return result;
 };
 
 // Do not edit below this line
