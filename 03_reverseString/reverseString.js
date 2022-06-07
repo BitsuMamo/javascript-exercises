338fd0dafb0835b822610911820a625d5253f4b9
Bitsu A. Mamo <bitsua.mamo@gmail.com>
2022-06-07 17:36:55 +0300

    Finished exercise 03. Funciton succesfully reveses strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(value) {
+  let length = value.length - 1;
+  let res = "";
+  while (length >= 0) {
+    res += value[length];
+    length--;
+  }
+  return res;
 };
 
 // Do not edit below this line
