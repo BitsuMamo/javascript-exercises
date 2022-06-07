3051625cd40f7385e4c91814e90e775fd9c4b99f
Bitsu A. Mamo <bitsua.mamo@gmail.com>
2022-06-07 17:57:40 +0300

    Finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if(typeof(start) != 'number' || typeof(end) != 'number'){
+    return "ERROR";
+  }
 
+  if(start < 0 || end < 0){
+    return "ERROR";
+  }
+
+  if(start > end){
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+
+  let res = 0;
+
+  while(start <= end){
+    res += end;
+    end--;
+  }
+
+  return res;
 };
 
 // Do not edit below this line
