f49bae8a8bb0ea011e00e091c17c251157ea52ba
Bitsu A. Mamo <bitsua.mamo@gmail.com>
2022-06-07 17:50:31 +0300

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const remove = function(array, value) {
+  const res = [];
+  array.forEach(element => {
+    if (element !== value) {
+      res.push(element);
+    }
+  });
+  return res;
+}
 
+const removeFromArray = function(array) {
+  for (let i = 1; i < arguments.length; i++) {
+    array = remove(array, arguments[i]);
+  }
+  return array;
 };
 
 // Do not edit below this line
