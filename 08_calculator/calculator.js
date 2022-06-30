758dea0d3f99151780d8c1da325409e6f6288a0d
Bitsu A. Mamo <bitsua.mamo@gmail.com>
2022-06-30 21:07:49 +0300

    Created all the functions with passing tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, number) => {
+    return total += number;
+  }, 0)
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, number) => {
+    return total *= number;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let ans = 1;
+  while(power !== 0){
+    ans *= base;
+    power --;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let ans = 1;
+  while(number > 0){
+    ans *= number;
+
+    number--;
+  }
+
+  return ans;
+
 };
 
 // Do not edit below this line
