badf8d35036f15e74a6fd7fac627fedd813f14c7
Bitsu A. Mamo <bitsua.mamo@gmail.com>
2022-07-02 15:25:51 +0300

    Created function with all passing test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const dp = {
+  0: 0,
+  1: 1,
 };
+const fibonacci = function(num) {
+  // console.log(`current number: ${num}\nfibonacci: ${dp[num]}`);
+  if( num < 0){
+    return "OOPS";
+  }
+
+  if(num in dp && num >= 0){
+    return dp[num];
+  }
+
+  return dp[num] = fibonacci(num - 2) + fibonacci(num - 1);
+};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
